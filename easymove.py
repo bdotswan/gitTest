c55a25e40044c964d38571ff05b1b9c0c1178cf9
bdotswan <b.swanov@gmail.com>
2021-03-19 16:15:57 +0900

    first pepper coding

---
+++ easymove.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# _*_ encoding: UTF-8 _*_
+
+from naoqi import ALProxy
+
+useSensorValues = False
+
+motion = ALProxy("ALMotion", "192.168.1.188", 9559)
+tts    = ALProxy("ALTextToSpeech", "192.168.1.188", 9559)
+motion.moveInit()
+
+for idx, num in enumerate([0.5, -0.5]):
+    id = motion.post.moveTo(num, 0, 0)
+    if (idx+1) % 2.0 == 0:
+        tts.say("I'm moving backward")
+        result = motion.getRobotPosition(useSensorValues)
+        print("Position after Move %d" % (idx+1), result)
+        motion.wait(id, 0)
+    else:
+        tts.say("I'm moving forward")
+        result = motion.getRobotPosition(useSensorValues)
+        print("Position after Move %d" % (idx+1), result)
+        motion.wait(id, 0)
+
+for idx, num in enumerate([0.5, -0.5]):
+    id = motion.post.moveTo(0, num, 0)
+    if (idx+1) % 2.0 == 0:
+        tts.say("I'm moving right")
+        result = motion.getRobotPosition(useSensorValues)
+        print("Position after Move %d" % (idx+1), result)
+        motion.wait(id, 0)
+    else:
+        tts.say("I'm moving left")
+        result = motion.getRobotPosition(useSensorValues)
+        print("Position after Move %d" % (idx+1), result)
+        motion.wait(id, 0)
+
+    
